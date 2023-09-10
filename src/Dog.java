2caa7a2cebf67ed172d02474d11eb3a5fa4d4cf5
danieldłuski <dluskidaniel@gmail.com>
2023-09-10 13:28:28 +0200

    Commit 23 - Dog Class Master

--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,8 @@
 public class Dog {
+    int ax = 5;
+    int bx = 5;
+    int cx = 5;
+    int dx = 5;
     public void somemethod(){
     // some content
         int a = 5;
--- src/SomeClass.java
+++ src/SomeClass.java
@@ -1,3 +1,6 @@
 public class SomeClass {
     //siema
+    public void method(){
+
+    }
 }
