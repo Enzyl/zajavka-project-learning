c7405b9c82fb349bee68e03d59d19afd395e6e22
danieldłuski <dluskidaniel@gmail.com>
2023-09-10 13:25:02 +0200

    Commit4-Temp-Branch

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,13 @@
 public class Dog {
     public void somemethod(){
     // some content
+        int a = 5;
+        int b = 1221;
+
     // some content
+    // some content
+    }
+    public void method(){
+
     }
 }
