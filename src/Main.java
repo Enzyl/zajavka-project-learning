dfb20572eb97bd9e6f86ac06de9eb69ff7588d95
danieldłuski <dluskidaniel@gmail.com>
2023-09-10 13:19:23 +0200

    another comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        public void method(){
+
+        };
+        System.out.println("Hello world3!");
+
         System.out.println("Hello world3!");
         System.out.println("Hello world!");
 
